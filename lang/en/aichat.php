48bd272417830fc1cd11ff0d2419c11d81497d75
Philipp Memmel <philipp.memmel@mailbox.org>
2026-01-21 13:57:30 +0100

    MBS-10358: Add extended description for activitychooser

--- lang/en/aichat.php
+++ lang/en/aichat.php
@@ -29,7 +29,18 @@ defined('MOODLE_INTERNAL') || die();
 $string['aichat:addinstance'] = 'Add an AI chat activity';
 $string['aichat:view'] = 'View the AI chat activity';
 $string['modulename'] = 'AI chat';
-$string['modulename_help'] = 'This activity provides the student with an AI chat bot.';
+$string['modulename_help'] = '###### Key features
+- Standard chatbot functionalities
+- Additional AI tools (image generation etc.) accessible by clicking the AI button in the input text area
+- Creating and selecting of personas (system prompts) for guiding the AI behavior
+- Configuration of the amount of messages that is being sent to the external AI system
+- Conversation history for each user
+
+###### Ways to use it
+- Standard chatbot for answering questions on specific topics
+- Tutor bot to help learners with exercises by defining a tutor persona
+- chatbot for learning *about* AI by experimenting with different personas and limiting length of context';
+$string['modulename_summary'] = 'This activity provides the students with an AI chatbot.';
 $string['modulenameplural'] = 'AI chats';
 $string['name'] = 'Name';
 $string['name_help'] = 'Name of this AI chat activity shown on the course page';
