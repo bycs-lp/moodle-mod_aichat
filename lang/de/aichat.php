d09d4d4f9fb988af859d16f3b791339fd89f867a
Philipp Memmel <philipp.memmel@mailbox.org>
2026-02-06 13:40:19 +0100

    MBS-10358: Add German translation

---
+++ lang/de/aichat.php
@@ -0,0 +1,50 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Language file for mod_aichat.
+ *
+ * @package     mod_aichat
+ * @copyright   2026 ISB Bayern
+ * @author      Philipp Memmel
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['aichat:addinstance'] = 'KI-Chat-Aktivität hinzufügen';
+$string['aichat:view'] = 'KI-Chat-Aktivität aufrufen';
+$string['modulename'] = 'KI-Chat';
+$string['modulename_help'] = '###### Zentrale Funktionen
+- Standard-Chatbot-Funktionen
+- Zusätzliche KI-Werkzeuge (Bildgenerierung etc.), erreichbar über den KI-Button im Eingabefeld
+- Erstellen und Auswählen von Personas (System-Prompts), um das Verhalten der KI zu steuern
+- Konfiguration der Anzahl an Nachrichten des Gesprächsverlaufs, die an das externe KI-System gesendet werden
+- Gesprächsverlauf für jede Nutzerin und jeden Nutzer
+
+###### Einsatzmöglichkeiten
+- Standard-Chatbot zum Beantworten von Fragen zu bestimmten Themen
+- Tutor-Chat zur Unterstützung von Lernenden bei Aufgaben durch Definition einer Tutor-Persona
+- Chatbot, um *über* KI zu lernen, z. B. durch Experimentieren mit unterschiedlichen Personas und Begrenzen der Länge des Gesprächsverlaufs';
+$string['modulename_summary'] = 'Diese Aktivität stellt den Lernenden einen KI-Chatbot zur Verfügung.';
+$string['modulenameplural'] = 'KI-Chats';
+$string['name'] = 'Name';
+$string['name_help'] = 'Name dieser KI-Chat-Aktivität, der auf der Kursseite angezeigt wird';
+$string['noaichatinstance'] = 'Es konnte keine Instanz von mod_aichat gefunden werden.';
+$string['pluginadministration'] = 'Verwaltung des KI-Chats';
+$string['pluginname'] = 'KI-Chat';
+$string['pluginname_userfaced'] = 'KI-Chat-Aktivität';
+$string['removeconversations'] = 'Alle Unterhaltungen löschen';
