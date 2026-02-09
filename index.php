cd2bf79522a1c0615ef7e52c8fc73dbe6a7ea515
Philipp Memmel <philipp.memmel@mailbox.org>
2026-02-09 06:20:40 +0100

    MBS-10572: Remove index.php and redirect to activityoverview page

--- index.php
+++ index.php
@@ -24,67 +24,7 @@
  */
 
 require('../../config.php');
-require_once('lib.php');
 
-$id = required_param('id', PARAM_INT);
-
-$course = $DB->get_record('course', ['id' => $id], '*', MUST_EXIST);
-require_course_login($course);
-
-$coursecontext = context_course::instance($course->id);
-
-$PAGE->set_url('/mod/aichat/index.php', ['id' => $id]);
-$PAGE->set_title(format_string($course->fullname));
-$PAGE->set_heading(format_string($course->fullname));
-$PAGE->set_context($coursecontext);
-
-echo $OUTPUT->header();
-
-$modulenameplural = get_string('modulenameplural', 'mod_aichat');
-echo $OUTPUT->heading($modulenameplural);
-
-$aichats = get_all_instances_in_course('aichat', $course);
-$usesections = course_format_uses_sections($course->format);
-
-$table = new html_table();
-$table->attributes['class'] = 'generaltable mod_index';
-
-if ($usesections) {
-    $table->head = [get_string('sectionname', 'format_' . $course->format), get_string('name')];
-    $table->align = ['left', 'left'];
-} else {
-    $table->head  = [get_string('name')];
-    $table->align = ['left'];
-}
-
-$aichatfound = false;
-
-foreach ($aichats as $aichat) {
-    $context = context_module::instance($aichat->coursemodule, IGNORE_MISSING);
-    if (!$context || !has_capability('mod/aichat:view', $context)) {
-        continue;
-    }
-
-    $aichatfound = true;
-    $linkcss = null;
-
-    if (!$aichat->visible) {
-        $linkcss = ['class' => 'dimmed'];
-    }
-
-    $link = html_writer::link(new moodle_url('/mod/aichat/view.php', ['id' => $aichat->coursemodule]), $aichat->name, $linkcss);
-
-    if ($usesections) {
-        $table->data[] = [get_section_name($course, $aichat->section), $link];
-    } else {
-        $table->data[] = [$link];
-    }
-}
-
-if (!$aichatfound) {
-    notice(get_string('noaichatinstance', 'mod_aichat'), new moodle_url('/course/view.php', ['id' => $course->id]));
-} else {
-    echo html_writer::table($table);
-}
-
-echo $OUTPUT->footer();
+$courseid = required_param('id', PARAM_INT);
+require_course_login($courseid);
+\core_courseformat\activityoverviewbase::redirect_to_overview_page($courseid, 'aichat');
